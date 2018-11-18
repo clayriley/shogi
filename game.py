91679c0c4a7a29bb056c112ee4d0663568d3d09c
Clay Riley <xxx@yyy.zzz>
2018-11-18 17:14:38 -0500

    Initial stub of Game class

---
+++ game.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@author: clayriley
+"""
+
+import pieces
+
+sente = False
+gote = not sente
+
+
+class Game(object):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
